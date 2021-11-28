1affe10b2b6443f2ab39e3e8d50d526611562db5
lafonth <lafonthub@gmail.com>
2021-11-28 23:45:06 +0200

    insert data - bug when inserting skills

--- bot.js
+++ bot.js
@@ -7,6 +7,51 @@ const {
   prefix
 } = require('./config.json');
 
+const playerPattern = {
+  type: "",
+  skillList: [{
+      order: 1,
+      name: "",
+      toy: ""
+    },
+    {
+      order: 2,
+      name: "",
+      toy: ""
+    },
+    {
+      order: 3,
+      name: "",
+      toy: ""
+    },
+    {
+      order: 4,
+      name: "",
+      toy: ""
+    }
+  ],
+  pet: ""
+};
+
+const patternSetup = {
+  name: "",
+  zone: {
+    name: "",
+    num: ""
+  },
+  playerList: []
+};
+
+patternSetup.playerList.push(playerPattern);
+patternSetup.playerList.push(playerPattern);
+patternSetup.playerList.push(playerPattern);
+patternSetup.playerList.push(playerPattern);
+
+var newSetup = {};
+var numPlayer = 0;
+var orderSkill = 0;
+var isOnGoing = 0;
+
 const myIntents = new Intents();
 
 myIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES);
@@ -22,15 +67,69 @@ client.once('ready', () => {
 });
 
 client.on('messageCreate', message => {
-  if(!message.content.startsWith(prefix) || message.author.bot) return;
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
 
-  if(command === 'ping'){
+  if (command === 'clear') {
+    newSetup = patternSetup;
+    numPlayer = 0;
+    orderSkill = 0;
+    isOnGoing = false;
+    message.channel.send('Reset DONE!');
+  } else if (command === 'ping') {
     message.channel.send('pong');
+  } else if (command === 'check') {
+    message.channel.send("Current setup: " + JSON.stringify(newSetup));
+  } else if (command === 'add') {
+    isOnGoing = true;
+    message.channel.send('Next Command : !boss nameBoss');
+  } else if (isOnGoing == true) {
+    if (command === 'boss') {
+      newSetup.name = args[0];
+      message.channel.send('Next Command : !zone nameZone numZone');
+    } else if (command === 'zone') {
+      newSetup.zone.name = args[0];
+      newSetup.zone.num = args[1];
+      message.channel.send('Next Command : !player numPlayer className');
+    } else if (command === 'player') {
+      numPlayer = parseInt(args[0]) - 1;
+      newSetup.playerList[numPlayer].type = args[1];
+      //affichage liste des sorts
+      message.channel.send('Next Command : !pet namePet');
+    } else if (command === 'pet') {
+      newSetup.playerList[numPlayer].type = args[0];
+      //affichage liste des sorts
+      message.channel.send('Next Command : !skill orderSkill nameSkill nameToy');
+    } else if (command === 'skill') {
+      orderSkill = parseInt(args[0]) - 1;
+      newSetup.playerList[numPlayer].skillList[orderSkill].name = args[1];
+      newSetup.playerList[numPlayer].skillList[orderSkill].toy = args[2];
+      if (isSkillListFilledUp(newSetup.playerList[numPlayer].skillList)) {
+        if (isPlayerListFilledUp(newSetup.playerList)) {
+          message.channel.send('Your setup is finshed!');
+        } else {
+          message.channel.send('Next Command : !player numPlayer className');
+        }
+      } else {
+        message.channel.send('Next Command : !skill orderSkill nameSkill nameToy');
+      }
+    }
   }
 })
+
+function isSkillListFilledUp(skillList) {
+  return skillList.every((skill) => {
+    return (skill.name !== "");
+  });
+}
+
+function isPlayerListFilledUp(playerList) {
+  return playerList.every((player) => {
+    return (isSkillListFilledUp(player.skillList));
+  });
+}
 
 // client.on('interactionCreate', async interaction => {
 // 	if (!interaction.isCommand()) return;
