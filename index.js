06cfc9124d67d976e769d6b5008e5044890d4c8d
lafonth <lafonthub@gmail.com>
2021-11-28 16:06:34 +0200

    reset

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const token = 'OTE0MTg0MzM2OTAxODE2MzUx.YaJWgg.bvl8Mq3tl3ec5RzXWx-_cga_4H0';
-const admin = require('firebase-admin');
-
-const serviceAccount = require('./firebase.json');
 
 client.once('ready', () => {
    console.log('Félicitations, votre bot Discord a été correctement initialisé !');
